3a509247f27aa6797b5f3e757fa804b378cad757
RpCpuX <95878942+RpCpuX@users.noreply.github.com>
2021-12-11 23:20:57 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -141,7 +141,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "Click [here](https://github.com) to open OUBnew-fortizer's GitHub page."
+        "Click [here](https://github.com/RpCpuX/Opo-aelah.git) to open OUBnew-fortizer's GitHub page."
     )
 
 
